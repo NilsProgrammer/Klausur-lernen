be40842b35ec56374d729443c318b3075bd2fd97
Nils <40488891+NilsProgrammer@users.noreply.github.com>
2024-04-22 21:37:04 +0200

    Update MatcherApp.cs

--- MatcherApp.cs
+++ MatcherApp.cs
@@ -18,17 +18,22 @@
 
         public void Match()
         {
+            List<IPerson> candidates = new List<IPerson>();
+
             foreach (IPerson person in People)
             {
                 foreach(IFruit fruit in Fruits)
                 {
                     bool matched = Matcher.Match(person, fruit);
+                    if (matched) candidates.Add(person);
                     if (matched)
                     {
                         Console.WriteLine("Matched " + person.Name + ": " + fruit.Name);
                     }
                 }
             }
+
+            candidates.ForEach(person => Console.WriteLine("Matched " + person.Name + ": " + person.FavoriteFruit));
         }
     }
 }
